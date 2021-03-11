07f9fc4d3d05ec93d1a1f7bce4df3fb8a9a0c155
Lindsey <linds.fonnes@gmail.com>
2021-03-11 13:56:07 -0700

    added sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,8 +8,12 @@ app.config["SECRET_KEY"] = "secret-secret-secret"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 questions = survey.questions
+
+@app.route("/session_response", methods=["POST"])
+def set_reponses():
+    session["responses"] = []
+    return redirect("/questions/0")
 
 @app.route("/")
 def show_survey():
@@ -19,9 +23,12 @@ def show_survey():
 
 @app.route("/questions/<int:num>")
 def show_question(num):
+    responses = session["responses"]
+
     if len(responses) != num:
         flash("Stop messing around! Invalid question")
         return redirect(f"/questions/{len(responses)}")
+
     elif len(responses) == len(questions):
         return redirect("/thank_you")
 
@@ -29,10 +36,15 @@ def show_question(num):
     
 @app.route("/answers", methods=["POST"])
 def add_answer():
-    answer = request.args.get("choice_val")
+    answer = request.form.get("choice_val")
+    
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
+
     if len(responses) == len(questions):
         return redirect("/thank_you")
+
     elif len(responses) < len(questions):
         return redirect(f"/questions/{len(responses)}")
 
